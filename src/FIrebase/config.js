b4858c9cc910678393bb898daeaa8c9811213c46
Pranav-0001 <pranavchakkalakkal@gmail.com>
2023-06-03 15:49:52 +0530

    env config

--- src/FIrebase/config.js
+++ src/FIrebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCP4tQj8QzshouOZ4qehJ81miRV2QbaF1w",
+    apiKey: process.env.react_app_key,
     authDomain: "rhythm-afc9d.firebaseapp.com",
     projectId: "rhythm-afc9d",
     storageBucket: "rhythm-afc9d.appspot.com",
     messagingSenderId: "1066326081346",
-    appId: "1:1066326081346:web:d6bf308b9272f60e4792db",
+    appId: process.env.react_app_api_id,
     measurementId: "G-Z0XX802Z39"
   };
 
